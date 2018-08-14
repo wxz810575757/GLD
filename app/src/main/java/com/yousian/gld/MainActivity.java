072fcb6669ef76891e2ff95a157575c35aefc1e3
DESKTOP-E1K6V2G\吴壮 <810575757@qq.com>
2018-08-14 15:52:59 +0800

    Initial commit

---
+++ app/src/main/java/com/yousian/gld/MainActivity.java
@@ -0,0 +1,23 @@
+package com.yousian.gld;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.classic.common.MultipleStatusView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
